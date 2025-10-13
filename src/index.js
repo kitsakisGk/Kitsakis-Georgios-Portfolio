c478925b9a723ebbb91f6a9d7f52313fd08daab7
George Kitsakis <57558604+kitsakisGk@users.noreply.github.com>
2025-10-14 02:15:07 +0300

    Fix routing for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Kitsakis-Georgios-Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
